4665ac75a719e79bc1c2e33fddfa43c3dd04438f
marisarze <flypetrov@yandex.ru>
2022-05-16 22:38:23 +0300

    attaching gflags to CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -2,10 +2,18 @@
 #include "SearchServer.h"
 #include "InvertedIndex.h"
 #include "Converter.h"
-int main() {
-    auto ii = InvertedIndex();
-    auto ss = SearchServer(ii);
-    auto conv = ConverterJSON("");
-    std::cout << "Hello, World!" << std::endl;
+
+#include <iostream>
+#include <gflags/gflags.h>
+
+DEFINE_string(name, "Tom", "name");
+DEFINE_int32(age, 18, "age");
+
+int main(int argc, char** argv) {
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    std::cout << "your name is : " << FLAGS_name
+              << ", your age is: " << FLAGS_name << std::endl;
+    gflags::ShutDownCommandLineFlags();
     return 0;
 }
+
