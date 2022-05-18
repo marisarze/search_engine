b4607cba6973d6f82904eb62bd0897bd88302cd4
marisarze <flypetrov@yandex.ru>
2022-05-18 08:22:40 +0300

    changing cli library to cli11

--- main.cpp
+++ main.cpp
@@ -4,16 +4,24 @@
 #include "Converter.h"
 
 #include <iostream>
-#include <gflags/gflags.h>
-
-DEFINE_string(name, "Tom", "name");
-DEFINE_int32(age, 18, "age");
+#include "CLI/CLI.hpp"
 
 int main(int argc, char** argv) {
-    gflags::ParseCommandLineFlags(&argc, &argv, true);
-    std::cout << "your name is : " << FLAGS_name
-              << ", your age is: " << FLAGS_name << std::endl;
-    gflags::ShutDownCommandLineFlags();
+    CLI::App app{"App description"};
+
+    std::string config_file = "./config.txt";
+    app.add_option("--config,-c", config_file, "Configuration file path");
+
+    std::string output_file = "./answers.txt";
+    app.add_option("--output,-o", output_file, "Output path for query response file");
+
+
+    CLI11_PARSE(app, argc, argv);
+
+    ConverterJSON converter(config_file);
+    converter.ValidateConfigFile();
+    converter.ShowConfigInfo();
+    InvertedIndex index;
+    index.UpdateDocumentBase(converter.GetTextDocuments());
     return 0;
 }
-
