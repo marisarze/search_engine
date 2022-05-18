2996736ac650dbb6695e0cf60219991dab30dc45
marisarze <35522569+marisarze@users.noreply.github.com>
2022-05-18 15:12:25 +0300

    Update Converter.h
    adding path variables for configuration, requests and responses

--- Converter/include/Converter.h
+++ Converter/include/Converter.h
@@ -11,11 +11,11 @@
 
 class ConverterJSON {
 private:
-    std::string folder = "";
+    std::string config_path, requests_path, answers_path;
     std::string read_open_file(std::ifstream &file);
 
 public:
-    ConverterJSON(std::string inFolder="");
+    ConverterJSON(std::string in_config_path, std::string in_requests_path, std::string in_answers_path);
     std::vector<std::string> GetTextDocuments();
     int GetResponsesLimit();
     std::vector<std::string> GetRequests();
